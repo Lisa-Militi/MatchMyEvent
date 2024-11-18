142ba25a2d3c4a05f847ba3642084c963e1bc90f
alicecpl <alice.caplain@gmail.com>
2024-11-18 18:11:00 +0100

    Update a_Home.py

--- pages/a_Home.py
+++ pages/a_Home.py
@@ -1,3 +1,21 @@
 import streamlit as st
 import session_state_handler as ss_handler
 
+st.set_page_config(
+    page_title="Welcome to MatchMyEvent!",
+    page_icon="✨",
+    layout="centered"
+)
+
+st.title("Welcome to MatchMyEvent!")
+
+image_path = "/Users/alice/Desktop/ClubsHSG.png"  
+st.image(image_path, caption="HSG Clubs", use_column_width=True)
+
+st.markdown(
+    """
+    *The Webpage to guide you through HSG campus events*  
+    *Do you feel overwhelmed by the too big amount of clubs and events proposed at HSG?* Don't worry, this page's for you.*
+    *We've created an algorithm that will perfectly match your preferences.*""",
+    unsafe_allow_html=True
+)
