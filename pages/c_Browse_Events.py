af5b909f334e9acf2f69cb29f13a40854bb42baf
Leo-R-17 <leo.ruem@gmail.com>
2024-11-19 12:15:08 +0100

    Update c_Browse_Events.py
    blueprint for further development; possible way of displaying instances of events

--- pages/c_Browse_Events.py
+++ pages/c_Browse_Events.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import session_state_handler as ss_handler
+import session_state_handler as sh
+import d_Event_Recommendations as ER
 
 
 
@@ -7,5 +8,16 @@ import session_state_handler as ss_handler
 
 st.subheader("Browse events")
 st.write("This is a placeholder for a potential expansion of the project; LOW PRIORITY")
+st.write("PRINT FIRST 10 INSTANCES FROM event_instances_list")
 
 
+'''
+def display_event_instance(event_instance):
+    st.subheader(ER.event_name)
+    st.write(event_date)
+    st.write(ER.event_description) #consider formatting: line breaks
+    st.write("Organizing Club:" club_name)
+'''
+#REQUIRED CLARIFICATIONS:
+#accessing instances of events
+#for-loop with sentinel (or similar) to cap number of events displayed
