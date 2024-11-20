c161ff35d62b85e2e783b6c72ced12e5a0a2b37b
Leo-R-17 <leo.ruem@gmail.com>
2024-11-20 18:44:08 +0100

    Update c_Browse_Events.py
    removed prov. code

--- pages/c_Browse_Events.py
+++ pages/c_Browse_Events.py
@@ -4,20 +4,3 @@ import d_Event_Recommendations as ER
 
 
 
-
-
-st.subheader("Browse events")
-st.write("This is a placeholder for a potential expansion of the project; LOW PRIORITY")
-st.write("PRINT FIRST 10 INSTANCES FROM event_instances_list")
-
-
-'''
-def display_event_instance(event_instance):
-    st.subheader(ER.event_name)
-    st.write(event_date)
-    st.write(ER.event_description) #consider formatting: line breaks
-    st.write("Organizing Club:" club_name)
-'''
-#REQUIRED CLARIFICATIONS:
-#accessing instances of events
-#for-loop with sentinel (or similar) to cap number of events displayed
