241120b939c240b1392db251832b68895cf9378e
alicecpl <alice.caplain@gmail.com>
2024-11-20 12:43:11 +0100

    Update d_Event_Recommendations.py

--- pages/d_Event_Recommendations.py
+++ pages/d_Event_Recommendations.py
@@ -5,11 +5,16 @@ import session_state_handler as sh
 class Event_profile:
 
     def __init__(self, event_name, event_date, club_name, event_type, event_description, event_keywords):
-        self.event_name = event_name #string
-        self.event_date = event_date
-        self.club_name = club_name #string
-        self.event_type = event_type #string
-        self.event_description = event_description #string
+        self.EventName = EventName #string
+        self.EventType = EventType #string
+        self.EventLanguage = EventLanguage #string
+        self.ClubName = ClubName #string
+        self.ClubCategory = ClubCategory #string
+        self.EventDescription = EventDescription #string
+        self.startDate = startDate #datetime
+        self.endDate = endDate #datetime
+        self.Location = Location #string
+
         self.event_keywords = event_keywords #list
 
         event_keywords = []
@@ -17,8 +22,13 @@ class Event_profile:
 
 class Club:
 
-    def __init__(self, club_name, club_keywords):
-        self.club_name = club_name #string
+    def __init__(self, ClubName, ClubCategory, ClubLanguage, SkillDevelopment, Interest):
+        self.ClubName = ClubName #string
+        self.ClubCategory = ClubCategory #string
+        self.ClubLanguage = ClubLanguage #string
+        self.SkillDevelopment = SkillDevelopment #string
+        self.Interest = Interest #string
+
         self.club_keywords = club_keywords #list
 
         club_keywords = []
