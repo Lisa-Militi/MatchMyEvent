475f025c011a0b076d5f01a16a257d25ce256e19
alicecpl <alice.caplain@gmail.com>
2024-11-20 12:59:19 +0100

    Update d_Event_Recommendations.py

--- pages/d_Event_Recommendations.py
+++ pages/d_Event_Recommendations.py
@@ -4,18 +4,15 @@ import session_state_handler as sh
 
 class Event_profile:
 
-    def __init__(self, event_name, event_date, club_name, event_type, event_description, event_keywords):
-        self.EventName = EventName #string
-        self.EventType = EventType #string
-        self.EventLanguage = EventLanguage #string
-        self.ClubName = ClubName #string
-        self.ClubCategory = ClubCategory #string
-        self.EventDescription = EventDescription #string
-        self.startDate = startDate #datetime
-        self.endDate = endDate #datetime
-        self.Location = Location #string
-
-        self.event_keywords = event_keywords #list
+    def __init__(self, event_name, event_type, event_language, club_name, club_name, club_category, event_description, start_date, end_date, location):
+        self.EventName = event_name #string
+        self.EventType = event_type #string
+        self.ClubName = club_name #string
+        self.ClubCategory = club_category #string
+        self.EventDescription = event_description #string
+        self.startDate = start_date #datetime
+        self.endDate = end_date #datetime
+        self.Location = location #string
 
         event_keywords = []
 
@@ -23,19 +20,19 @@ class Event_profile:
 class Club:
 
     def __init__(self, ClubName, ClubCategory, ClubLanguage, SkillDevelopment, Interest):
-        self.ClubName = ClubName #string
-        self.ClubCategory = ClubCategory #string
-        self.ClubLanguage = ClubLanguage #string
-        self.SkillDevelopment = SkillDevelopment #string
-        self.Interest = Interest #string
+        self.ClubName = club_name #string
+        self.ClubCategory = club_category #string
+        self.ClubLanguage = club_language #string
+        self.SkillDevelopment = skill_development #string
+        self.Interest = interest #string
 
         self.club_keywords = club_keywords #list
 
         club_keywords = []
 
 
-#TEST INSTANCES
-test_event = Event_profile('Symposium', 'club_name', 'panel_discussion', 'this is where the description goes as a string', ['international', 'politics', 'economics'])
+#INSTANCES
+Event1 = Event_profile('üß† Memory Clear Jan. 30th', 'club_name', 'panel_discussion', 'this is where the description goes as a string', ['international', 'politics', 'economics'])
 
 test_club = Club('HIC', ['finance', 'economics', 'banking'])
 
