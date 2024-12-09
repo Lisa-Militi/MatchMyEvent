dfad2cb32c9b7dd297cd90abbae5278134e91d25
alicecpl <alice.caplain@gmail.com>
2024-12-09 09:58:29 +0100

    Update a_Home.py

--- pages/a_Home.py
+++ pages/a_Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import session_state_handler as ss_handler
 
 
-st.title(sparkles:"Welcome to MatchMyEvent!":sparkles:)
+st.title(":sparkles:Welcome to MatchMyEvent!:sparkles:")
 
 st.subheader("The Webpage to guide you through HSG campus events")
 st.subheader("Do you feel overwhelmed by the too big amount of clubs and events proposed at HSG?")
