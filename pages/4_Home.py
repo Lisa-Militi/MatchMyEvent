27fc7bc82057d7b3761f3e23a0f3b0083ed73b0f
alicecpl <alice.caplain@gmail.com>
2024-12-11 11:41:30 +0100

    Update 4_Home.py

--- pages/4_Home.py
+++ pages/4_Home.py
@@ -18,8 +18,6 @@ st.markdown(
 st.markdown("<h1 class=""centered"">Welcome to MatchMyEvent :) </h1>", unsafe_allow_html=True)
 st.markdown('<p class="centered">The webpage to guide you through HSG campus events</p>', unsafe_allow_html=True)
 st.markdown('<h4 class="centered">Do you feel overwhelmed by the too big amount of clubs and events proposed at HSG?</h4>', unsafe_allow_html=True)
-st.markdown('<h4 class="centered">Don\'t worry, this page\'s for you</h4>', unsafe_allow_html=True)
-st.markdown('<p class="centered">We\'ve created an algorithm that will perfectly match your preferences</p>', unsafe_allow_html=True)
 
 # URL brute de l'image
 image_url = "MatchmyEvent Logo.png"
@@ -27,3 +25,5 @@ image_url = "MatchmyEvent Logo.png"
 # Afficher l'image
 st.image(image_url, use_column_width=True)
     
+st.markdown('<h4 class="centered">Don\'t worry, this page\'s for you</h4>', unsafe_allow_html=True)
+st.markdown('<p class="centered">We\'ve created an algorithm that will perfectly match your preferences</p>', unsafe_allow_html=True)
