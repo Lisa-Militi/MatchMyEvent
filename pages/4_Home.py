ffaf7a7cc761146d4531745fda3e797e4d3ef64e
alicecpl <alice.caplain@gmail.com>
2024-12-11 11:40:13 +0100

    Update 4_Home.py

--- pages/4_Home.py
+++ pages/4_Home.py
@@ -22,7 +22,7 @@ st.markdown('<h4 class="centered">Don\'t worry, this page\'s for you</h4>', unsa
 st.markdown('<p class="centered">We\'ve created an algorithm that will perfectly match your preferences</p>', unsafe_allow_html=True)
 
 # URL brute de l'image
-image_url = "MatchMyEvent Logo.tiff"
+image_url = "MatchmyEvent Logo.png"
 
 # Afficher l'image
 st.image(image_url, use_column_width=True)
